1d801f906653d77c343d10eee7cd0ad0aca312b2
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-25 19:42:48 -0800

    Assert installer succeed or abort the workflow.

--- setup-bazel.sh
+++ setup-bazel.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-BAZEL_DOWNLOAD_PREFIX="https://github.com/bazelbuild/bazel/releases/download"
-ARCH="x86_64"
 VERSION=$1
+ARCH="x86_64"
+BAZEL_DOWNLOAD_PREFIX="https://github.com/bazelbuild/bazel/releases/download"
 
 kernelName="$(uname -s)"
 case "$kernelName" in
-    Darwin*)    installBazel=MacSetup;;
-    Linux*)     installBazel=LinuxSetup;;
+    Darwin*)    installBazel=CommonSetup;;
+    Linux*)     installBazel=CommonSetup;;
     *)          installBazel=WindowsSetup;;
 esac
 
-MacSetup() {
+CommonSetup() {
     curl -fsSL ${BAZEL_DOWNLOAD_PREFIX}/${VERSION}/bazel-${VERSION}-installer-darwin-${ARCH}.sh \
         -o bazel-installer.sh
+    if [[ $? != 0 ]]; then
+        echo "Failed to download bazel installer"
+        exit 1
+    fi
     chmod +x bazel-installer.sh
     sudo ./bazel-installer.sh
-}
-
-LinuxSetup() {
-    curl -fsSL ${BAZEL_DOWNLOAD_PREFIX}/${VERSION}/bazel-${VERSION}-installer-linux-${ARCH}.sh \
-        -o bazel-installer.sh
-    chmod +x bazel-installer.sh
-    sudo ./bazel-installer.sh
+    rm bazel-installer.sh
 }
 
 WindowsSetup() {
     curl -fsSL ${BAZEL_DOWNLOAD_PREFIX}/${VERSION}/bazel-${VERSION}-windows-${ARCH}.exe \
         -o bazel.exe
+    if [[ $? != 0 ]]; then
+        echo "Failed to download bazel exe"
+        exit 1
+    fi
 }
 
-# Invoke installation method
 $installBazel
