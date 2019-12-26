b3a8607960f4ce729d5c2fae5a9bf767b3a3bfa6
Abhinav Singh <mailsforabhinav@gmail.com>
2019-12-25 18:52:49 -0800

    No es6

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import { setFailed } from '@actions/core';
-import github from '@actions/github';
-import { exec } from 'child_process';
-import { join } from 'path';
+const core = require('@actions/core');
+const exec = require('child_process').exec;
+const path = require('path');
 
 try {
-    const setupBazel = join(__dirname, 'setup-bazel.sh');
+    const setupBazel = path.join(__dirname, 'setup-bazel.sh');
     const version = core.getInput('version');
     exec(setupBazel + " " + version, function(error, stdout, stderr) {
         if (error) {
-            setFailed(error.message);
+            core.setFailed(error.message);
         }
     });
 } catch (error) {
-    setFailed(error.message);
+    core.setFailed(error.message);
 }
